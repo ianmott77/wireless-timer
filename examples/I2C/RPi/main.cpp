1def03d0f4d35801886773a61fe136e2f0b590cd
ianmott77 <mott.ian77@gmail.com>
2017-05-21 13:00:54 -0400

    updated example with comments for clarity

--- examples/I2C/RPi/main.cpp
+++ examples/I2C/RPi/main.cpp
@@ -2,19 +2,26 @@
 #include <ctime>
 
 int main(){
+    //create a connection manager object so we can manage connections
 	ConnectionManager * cm = new ConnectionManager();
+    
+    //connect to the i2c connection
 	if(cm->connect(I2C)){
 		std::cout << "Connected" << std::endl;
-		
 		std::string result = "Hello World!";
+        
+        //create packet object Packet(void* data, std::string dataType, int size)
 		Packet *  p = new Packet((char*)result.c_str(), "string", result.size());
+        
+        //send the packet accros the current connection (I2C)
 		if(cm->sendData(p)){
 			std::cout << "Sent" << std::endl;
 		}
 		
-		//sleep(1);
-		
+        //read data from slave which returns a new packet
 		Packet * p2 = cm->readData();
+        
+        //check for the data type so we can properly convert it
 		if(p2->getDataType() == "string"){
 			std::cout << (char*) p2->getData() << std::endl;
 		}else if(p2->getDataType() == "long"){
